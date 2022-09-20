f4bc824034d0b59f9c4155a18c83d8ad6d47e6ee
GiulianoPairone <yuliars.pairone@gmail.com>
2022-09-20 20:00:10 -0300

    Actulizacion 20/9

---
+++ PraJs/1/js/1.js
@@ -0,0 +1,15 @@
+var Numero1 = document.getElementById('num1');
+var Numero2 = document.getElementById('num2');
+var Numero3 = document.getElementById('num3');
+var Resultado = document.getElementById("resultado")
+var boton = document.getElementById('boton');
+
+function mayor(){
+    if (Numero1>Numero2){
+        if(Numero1>Numero3){
+            function showInputValue(){
+                console.log("El numero mayor es el numero")
+            }
+        };
+    };
+};
